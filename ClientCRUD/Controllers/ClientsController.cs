07b688d4b179920a483fd7614f9dd58cccb04467
Mktm <mody.kane60100@gmail.com>
2021-09-25 15:27:06 +0200

    Ajoutez des fichiers projet.

---
+++ ClientCRUD/Controllers/ClientsController.cs
@@ -0,0 +1,65 @@
+﻿using ClientCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClientCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        private readonly ClientApiContext _context;
+
+        public ClientsController(ClientApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClientById(int id)
+        {
+            var client = await _context.Clients.Where(c => c.Id.Equals(id)).FirstOrDefaultAsync();
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return client;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Client>> CreateClient(Client client)
+        {
+             // Validation des données
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetClientById), new { id = client.Id }, client);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int Id)
+        {
+            var client = await _context.Clients.FindAsync(Id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ ClientCRUD/Models/ClientApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientCRUD.Models
+{
+    public class ClientApiContext : DbContext
+    {
+        
+        public ClientApiContext(DbContextOptions <ClientApiContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
